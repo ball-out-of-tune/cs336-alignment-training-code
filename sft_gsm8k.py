88132b2bfaecae60d709f56a95476d7d37187237
Zhong Yichen <1687436023@qq.com>
2025-12-11 00:26:08 +0800

    limit vllm max_model_len to 4096

--- sft_gsm8k.py
+++ sft_gsm8k.py
@@ -17,7 +17,7 @@ from vllm import LLM, SamplingParams
 from vllm.model_executor import set_random_seed as vllm_set_random_seed
 from unittest.mock import patch
 
-def init_vllm(model_id: str, device: str, seed: int, gpu_memory_utilization: float = 0.85):
+def init_vllm(model_id: str, device: str, seed: int, gpu_memory_utilization: float = 0.85, max_model_len: int = 4096,):
     vllm_set_random_seed(seed)
     world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
     profiling_patch = patch(
@@ -31,6 +31,7 @@ def init_vllm(model_id: str, device: str, seed: int, gpu_memory_utilization: flo
             dtype=torch.bfloat16,
             enable_prefix_caching=True,
             gpu_memory_utilization=gpu_memory_utilization,
+            max_model_len=max_model_len,
         )
 
 def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
